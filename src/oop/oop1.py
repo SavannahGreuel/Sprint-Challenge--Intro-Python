6968f9dc0844ec8b920bddb31616c55c9efd0ec7
SavannahGreuel <smallidgesavannah@gmail.com>
2019-02-15 09:23:59 -0700

    oop1 complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle- base
+class Vehicle:
+    pass
+
+# FlightVehicle(vehicle)
+class FlightVehicle(Vehicle):
+    pass
+
+# Starship(FlightVehicle)
+class Starship(FlightVehicle):
+    pass
+
+# Airplane(FlightVehicle)
+class Airplane(FlightVehicle):
+    pass
+
+# GroundVehicle(vehicle)
+class GroundVehicle(Vehicle):
+    pass
+
+# Car(GroundVehicle)
+class Car(GroundVehicle):
+    pass
+
+# Motercycle(GroundVehicle)
+class Motorcycle(GroundVehicle):
+    pass
+
+
